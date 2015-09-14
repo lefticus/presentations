36d94939cbb693e9a3406f4f3f29a06a45dae519
Jason Turner <jason@emptycrate.com>
2015-09-14 15:11:45 -0600

    Move to cmake for building presentation materials

---
+++ CrazyEasyScriptingWithChaiScript.md
@@ -0,0 +1,26 @@
+# ChaiScript
+
+It's crazy easy to use. 
+
+
+# Jason Turner
+
+ * http://chaiscript.com
+ * http://cppbestpractices.com
+ * http://github.com/lefticus
+ * http://cppcast.com
+ * @lefticus
+ * Independent Contractor - *Always looking for new clients*
+
+
+# Jason Turner
+
+ * http://chaiscript.com
+ * http://cppbestpractices.com
+ * http://github.com/lefticus
+ * http://cppcast.com
+ * @lefticus
+ * Independent Contractor - *Always looking for new clients*
+
+
+
--- build.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/sh
-
-pandoc -t revealjs -s ThinkingPortable.md -o ThinkingPortable.html -A revealjs-header.html --css reveal.js/css/theme/lefticus.css
-wkhtmltopdf --footer-right "Jason Turner Copyright 2015 @lefticus [page] / [topage]" -O Landscape -s Letter "file:///`pwd`/ThinkingPortable.html?print-pdf" ThinkingPortable.pdf
-
-pandoc -t revealjs -s CurrentStateOfStaticAnalysis.md -o CurrentStateOfStaticAnalysis.html -A revealjs-header.html --css reveal.js/css/theme/lefticus.css
-wkhtmltopdf --footer-right "Jason Turner Copyright 2015 @lefticus [page] / [topage]" -O Landscape -s Letter "file:///`pwd`/CurrentStateOfStaticAnalysis.html?print-pdf" CurrentStateOfStaticAnalysis.pdf
-
